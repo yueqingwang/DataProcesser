e4c7ba46bf837e2243794ac32cdbb264348458d1
yueqingwang89 <wangyueqing@handan.fmsh.com>
2016-09-23 16:42:30 +0800

    verson 1.1

---
+++ DataProcessApp.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 23 13:20:16 2016
+
+@author: wangyueqing
+"""
+
+import sys
+from PyQt4 import QtGui, QtCore
+
+import DataProcessUi
+
+class DataProcessApp(QtGui.QMainWindow, DataProcessUi.Ui_MainWindow):
+    def __init__(self, parent = None):
+        super(self.__class__, self).__init__()
+        self.setupUi(self)
+        self.setupMenuBar()
+        self.setupToolBar()
+        
+    def setupMenuBar(self):
+        fileMenu = self.menuBar.addMenu('&File')
+        openAction = fileMenu.addAction(QtGui.QIcon('./images/file/fileopen.png'),'&open')
+        openAction.setShortcut('Ctrl+O')
+        saveAction = fileMenu.addAction(QtGui.QIcon('./images/file/filesave.png'),'&save')
+        saveAction.setShortcut('Ctrl+S')
+        saveasAction = fileMenu.addAction(QtGui.QIcon('./images/file/filesaveas.png'),'&saveas')
+        openAction.triggered.connect(self.openActionSlot)
+        editMenu = self.menuBar.addMenu('&Edit')
+
+
+        
+    def setupToolBar(self):               
+        self.toolBar.addAction(QtGui.QIcon('./images/console/run_small.png'),'&File')
+        
+    def openActionSlot(self):
+        name = QtGui.QFileDialog.getOpenFileName(self, 'Open file','./',"Datlog files (*.dlg)")
+        print(name)
+        
+def main():
+    app = QtGui.QApplication(sys.argv)
+    ex = DataProcessApp()
+    ex.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
---
+++ DataProcessUi.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'DataProcessUi.ui'
+#
+# Created by: PyQt4 UI code generator 4.11.4
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName(_fromUtf8("MainWindow"))
+        MainWindow.resize(800, 600)
+        MainWindow.setAccessibleName(_fromUtf8(""))
+        self.centralwidget = QtGui.QWidget(MainWindow)
+        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusBar = QtGui.QStatusBar(MainWindow)
+        self.statusBar.setObjectName(_fromUtf8("statusBar"))
+        MainWindow.setStatusBar(self.statusBar)
+        self.menuBar = QtGui.QMenuBar(MainWindow)
+        self.menuBar.setGeometry(QtCore.QRect(0, 0, 800, 23))
+        self.menuBar.setObjectName(_fromUtf8("menuBar"))
+        MainWindow.setMenuBar(self.menuBar)
+        self.toolBar = QtGui.QToolBar(MainWindow)
+        self.toolBar.setObjectName(_fromUtf8("toolBar"))
+        MainWindow.addToolBar(QtCore.Qt.TopToolBarArea, self.toolBar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow", None))
+        self.toolBar.setWindowTitle(_translate("MainWindow", "toolBar", None))
+
