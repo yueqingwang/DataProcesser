740972aa6500f3adb7443c87bc7da3cb14fa0b17
yueqingwang89 <wangyueqing@handan.fmsh.com>
2016-10-11 17:06:58 +0800

    增加注释

--- test_flow.py
+++ test_flow.py
@@ -1,8 +1,28 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sun Oct  9 13:36:34 2016
-
 @author: wangyueqing
+
+状态机说明：
+self.fsm = {
+'s0':判断是否为test flow的起始，是则开始读头文件s1，否则跳行重复执行s0
+'s1':判断是否flow的头文件是否已经读取完，是则进行头文件处理，并进入s2，否则继续读s1
+'s2':判断是否为test的起始，若是，则开始test数据的处理s3，否则进入s8
+'s3':判断是否为pmu test的起始，若是，则开始pmu数据的处理s4，否则进入s5
+'s4':判断是否为pmu数据格式，若是进行处理，且再次对下一行进行同样判断s4
+否则开始返回s3
+'s5':判断是否为vlog test的起始，若是，则开始vlog数据的处理s6，否则进入s7
+'s6':判断是否为vlog数据格式，若是进行处理，且再次对下一行进行同样判断s6
+否则开始返回s5
+'s7':判断是否为自定义的数据格式，若是进行处理，且再次对下一行进行同样判断s7
+否则开始返回s2，判断是否为新的test的起始，若是，按照新test的流程处理
+'s8':判断是否为flow的结束，若是进行处理，跳转到s0,执行下一个flow的处理。否则跳转到s9
+'s9':与s0的判断方法相同，判断是否为test flow的起始，这是由于在测试中发现会有执行完头文件
+就开始下一个flow的现象出现。若是，则跳转到s0，且此处不读入下一行数据。若不是，则可能是自
+定义的数据之后有其他的格式。首先进入s10
+'s10':判断是否是 pmu 或是 vlog 测试，是则进入s3开始处理，否则，读取下一行进入s7处理
+}
+
 """
 
 import pandas as pd
@@ -44,6 +64,9 @@ m7 = re.compile(r)
 r = 'Kalos2 '
 m8 = re.compile(r)
 
+r = 'Test.Mods.Site.PHY'
+m9 = re.compile(r)
+
 def fsm_fun(argfunc,arg):
     argfunc(arg)
 
@@ -75,11 +98,11 @@ class TestResultTree(object):
         's5':[m5,'s6','s7',True,False,self.dummy_loop,self.dummy_loop],
         's6':[m6,'s6','s5',True,False,self.update_vlog_record,self.dummy_loop],
         's7':[m7,'s7','s2',True,False,self.update_dlg_record,self.dummy_loop],
-        's8':[m8,'s0','s7',True,True,self.update_head_info,self.dummy_loop]
+        's8':[m8,'s0','s9',True,False,self.update_head_info,self.dummy_loop],
+        's9':[m0,'s0','s10',False,True,self.update_head_info,self.dummy_loop],
+        's10':[m9,'s3','s7',False,True,self.update_head_info,self.dummy_loop]
         }
-        
         self.setupTree(pattern)
-        
         
         
     def setupTree(self,pattern):
@@ -140,10 +163,12 @@ class TestResultTree(object):
         self.head_info.append(self.flow_id)
     
     def update_pmu_record(self,line):
-        self.pmu_records.append([self.flow_id,self.test_id,line])
+        pmu_record = [self.flow_id,self.test_id] + list(m4.match(line).groups())
+        self.pmu_records.append(pmu_record)
         
     def update_vlog_record(self,line):
-        self.vlog_records.append([self.flow_id,self.test_id,line])
+        vlog_record = [self.flow_id,self.test_id] + list(m6.match(line).groups())
+        self.vlog_records.append(vlog_record)
     
     def update_dlg_record(self,line):
         self.dlg_records.append([self.flow_id,self.test_id,line])
@@ -156,7 +181,7 @@ class TestResultTree(object):
 if __name__ == '__main__':
     
     Tree = TestResultTree('D:\\python\\DataProcesser\\1543W','.+\.dlg')
-    print(Tree.treelist)
+#    print(Tree.treelist)
 #    for i in Tree.pmu_records:
 #        print(i)
     #print(Tree.pmu_records)
